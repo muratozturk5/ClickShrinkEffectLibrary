b68ec3cb669f583bd7af68ad5d74ad4569183077
devearthapp <62841905+devearthapp@users.noreply.github.com>
2022-08-01 20:43:31 +0300

    example updated

--- app/src/main/java/com/muratozturk/clickshrinkeffectlibrary/MainActivity.kt
+++ app/src/main/java/com/muratozturk/clickshrinkeffectlibrary/MainActivity.kt
@@ -6,7 +6,6 @@ import com.muratozturk.click_shrink_effect.applyClickShrink
 import com.muratozturk.clickshrinkeffectlibrary.databinding.ActivityMainBinding
 
 
-
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tryButton.applyClickShrink()
+        binding.button.applyClickShrink()
+        binding.textView.applyClickShrink(0.80f, 150L)
+        binding.cardView.applyClickShrink(0.75f, 150L)
+        binding.imageView.applyClickShrink(0.50f, 250L)
     }
 }
